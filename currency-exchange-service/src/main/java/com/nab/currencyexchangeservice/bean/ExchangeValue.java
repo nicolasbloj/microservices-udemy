e833635a4ee73aedca6e5f510ce5100ca683124c
Nicolas Bloj <nicolasbloj@gmail.com>
2017-12-20 16:20:51 -0300

    exchangevalue

---
+++ currency-exchange-service/src/main/java/com/nab/currencyexchangeservice/bean/ExchangeValue.java
@@ -0,0 +1,43 @@
+package com.nab.currencyexchangeservice.bean;
+
+import java.math.BigDecimal;
+
+public class ExchangeValue {
+	
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal covertionMultiple;
+	
+	protected ExchangeValue() {
+		
+	}
+	
+	public ExchangeValue(Long id, String from, String to, BigDecimal covertionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.covertionMultiple = covertionMultiple;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public BigDecimal getCovertionMultiple() {
+		return covertionMultiple;
+	}
+	
+	
+	
+	
+}
