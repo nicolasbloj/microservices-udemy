682a58e28b06657ab929025c2561f71ba67b1b22
Nicolas Bloj <nicolasbloj@gmail.com>
2017-12-27 18:05:29 -0300

    ribbon

--- currency-conversion-service/src/main/java/com/nab/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/nab/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,15 @@
 package com.nab.currencyconversionservice.proxy;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.nab.currencyconversionservice.bean.CurrencyConversion;
 
-@FeignClient(name="currency-exchange-service",url="localhost:8000")
+//@FeignClient(name="currency-exchange-service",url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
