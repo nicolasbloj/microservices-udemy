b4ebf779179f0d5156f6d47a0cc3bef3fef6e2fc
Nicolas Bloj <nicolasbloj@gmail.com>
2017-12-27 23:19:54 -0300

    call - eureka and ribbon

--- currency-exchange-service/src/main/java/com/nab/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/nab/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -12,16 +12,20 @@ import com.nab.currencyexchangeservice.repository.ExchangeValueRepository;
 @RestController
 public class CurrencyExchangeController {
 
-	@Autowired
-	Environment environment;
+  @Autowired
+  Environment environment;
 
-	@Autowired
-	ExchangeValueRepository exchangeValueRepository;
+  @Autowired
+  ExchangeValueRepository exchangeValueRepository;
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
-		exchangeValue.setPort((Integer.parseInt(environment.getProperty("local.server.port"))));
-		return exchangeValue;
-	}
+  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+    ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+    exchangeValue.setPort((Integer.parseInt(environment.getProperty("local.server.port"))));
+    // Usamos el puerto para determinar que instancia nos esta respondiendo cuando hagamos uso de
+    // este servicio
+    // ya que estaremos trabajando con LOAD BALANCE - RIBBON
+
+    return exchangeValue;
+  }
 }
