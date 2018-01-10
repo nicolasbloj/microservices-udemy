722b1d73c2f2a5ddfc2100f41a318a172f9a6d63
Nicolas Bloj <nicolasbloj@gmail.com>
2018-01-10 11:10:12 -0300

    connecting microservices to zipkin

--- zipkin-distributed-tracing-server/src/main/java/com/nab/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
+++ zipkin-distributed-tracing-server/src/main/java/com/nab/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -3,6 +3,9 @@ package com.nab.zipkindistributedtracingserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinDistributedTracingServerApplication {
 
