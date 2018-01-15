1280c31b6be2e264b06be09000b80533a8e3eb27
Nicolas Bloj <nicolasbloj@gmail.com>
2018-01-15 12:18:00 -0300

    Hystrix

--- limits-service/src/main/java/com/nab/limitsservice/LimitsConfigController.java
+++ limits-service/src/main/java/com/nab/limitsservice/LimitsConfigController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nab.limitsservice.bean.LimitConfig;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigController {
@@ -19,5 +20,16 @@ public class LimitsConfigController {
 	public LimitConfig retrieveLimitsForConfiguration(){
 		return new LimitConfig(config.getMinimum(),config.getMaximum());
 	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfig retrieveConfiguration(){
+		throw new RuntimeException("Not available");
+	}
+ 
+	public LimitConfig fallbackRetrieveConfiguration(){
+		return new LimitConfig(1,999);
+	}
+	
 }
 	
